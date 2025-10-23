52dde9039e44615d1f87206a322b66b596b466bb
mohansai1997 <mohansai01234@gmail.com>
2025-10-23 23:18:23 +0530

    remove unnecessary files

--- lib/models/models.dart
+++
@@ -1,3 +0,0 @@
-// Models exports for Gita Connect
-export 'youtube_short.dart';
-export 'lecture_video.dart';
--- lib/widgets/widgets.dart
+++
@@ -1,5 +0,0 @@
-// Widgets exports for Gita Connect
-export 'shorts/shorts_section.dart';
-export 'quote/quote_section.dart';
-export 'gallery/gallery_section.dart';
-export 'lectures/lectures_section.dart';
